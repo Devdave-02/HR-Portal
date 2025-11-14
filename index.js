7d8ac3c96be3ec2000a984ba73ebebb6e9595bc3
Devdave-02 <davidokanla69@gmail.com>
2025-11-14 15:15:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import express from "express";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/AuthRoutes.js";
 import userRoutes from "./routes/UserRoutes.js";
+import cors from "cors";
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -16,3 +19,4 @@ app.use("/api", userRoutes);
 connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
